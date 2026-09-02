98cda03f3eb015bb3a972e083f83b18bc917ad82
LORENZO BORGES RIBEIRO DA ROSA <04499273005@senacrs.edu.br>
2026-09-02 20:43:42 -0300

    iniciando uc2

---
+++ Variavel/conteudo.js
@@ -0,0 +1,86 @@
+
+//=============================
+// VARIAVEL
+//=============================
+// Variavel e um espaco na memoria para guardar informaoes
+
+// let pode mudar 
+// const nao(constante)
+
+//let nome = "Rafael Lopes Gomes Moraes Fek Roajas Malta Vargas Borges Silva Vedoy Cavalari Wildner  Quevedo Marques"
+
+//nome = "Jovem Eduardo"
+//nome = "yuhan"
+
+// console.log() uso para mostrar algo na saida 
+// LET - Usamos LET quando quando queremos criar uma variavel cujo valor PODERA SER ALTERADO postriormente.
+
+//let nomeCidade = "Sao Leo"
+//nomeCidade =  "Quarai"
+// A variavel nomeCidade vai retornar a ultima informacao = quarai
+//console.log(nomeCidade)
+
+
+// CONST - Usamos CONST quando queremos criar uma variavel cujo valor NAO PODERA SER ALTERADO postriormente.
+
+//====================
+
+//const pi =3.14
+// se tentarmos alterar o valor de pi, teremos um erro  
+//
+//console.log(pi)
+
+//====================
+
+/*
+NOMES DE VARIAVEIS
+-------------------
+
+EVITE
+let x = "Maria"
+let y = 25
+let z = true   
+
+
+PREFIRA
+let nomeAluno = "maria"
+let idadeAluno = 25
+let alunoMatriculado = true
+    
+    */
+//==============================
+
+
+// TIPOS DE VARIAVEIS
+
+// NUMBER
+
+const idadeAlunoTDS = 23
+const altura =1.79
+const temperatura = -23
+const quantidadeDeAlunos = 14
+
+// pra mostrar na saida usamos console.log
+console.log(idadeAlunoTDS)
+console.log(altura)
+console.log(temperatura)
+console.log(quantidadeDeAlunos) 
+
+// STRING
+
+// STRING representa TEXTO (conjundo de caracteries). Sempre colocamos entre aspas duplas a informacao "".
+let escola = "Senac"
+const linguagemProgrramacao = "JavaScript" 
+console.log(escola)
+console.log(linguagemProgrramacao)
+
+// BOOLEAN 
+
+// Uma variavel BOOLEANA representa uma informacao logica.
+// representa pergunta como> O usuario esta logado? O pegamento foi aproado? | Sempre como TRUE (verdadeiro) ou FALSE (falso).
+
+let usuarioLogado = true   
+let pagamentoAprovado = false
+let LorenzoMaiorIdade = false
+
+console.log(usuarioLogado, pagamentoAprovado, LorenzoMaiorIdade)
---
+++ Variavel/exercicio1.js
@@ -0,0 +1,22 @@
+//const nome = "Lorenzo"
+//const sobreNome = "Borgers"
+//let idade = "17"
+//let estudante = true
+
+//console.log (`Olá, meu nome é ${nome} ${sobreNome}, tenho ${idade} anos e sou estudante: ${estudante}.`)
+
+//========================================
+
+//const nomeDoCachorro = "luki"
+//let numeroDeOssos = 5   
+//let numeroDeBolinhas = 3
+//let OCachorroEstaFeliz = true
+//console.log (`O nome do cachorro é ${nomeDoCachorro}, ele tem ${numeroDeOssos} ossos e ${numeroDeBolinhas} bolinhas. O cachorro está feliz? ${OCachorroEstaFeliz}.`)
+
+const nomeDaNave = "alvin"
+let numeroDeTripulantes = 67
+let destinoDaMissao = "universo Marvel"  
+let tempoEstimadoDaViagem = "212h"
+let AMissaoFoiLancada = true
+
+console.log (`O nome da nave é ${nomeDaNave}, ela tem ${numeroDeTripulantes} tripulantes, o destino da missão é ${destinoDaMissao}, o tempo estimado da viagem é de ${tempoEstimadoDaViagem} e a missão foi lançada? ${AMissaoFoiLancada}.`)
